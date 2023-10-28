7f64e3c6c9ac80a5df5c90e935b5bb015f53c291
aaron4345-creator <149237484+aaron4345-creator@users.noreply.github.com>
2023-10-28 23:00:24 +0800

    Add files via upload

--- 最大公因数1.cpp
+++ 最大公因数1.cpp
@@ -4,14 +4,17 @@ int main()
 int i=0;
 int j=0;
 int a=1;
+int b=0;
 scanf("%d",&i);
 scanf("%d",&j);
 for(a=1;a<=i;a++){
 	if(i/a*a==i){
 		if(j/a*a==j){
-		}	   	
+		b=a;
+		}
+			   	
 	}
 }
-printf("%d",a-1);
+printf("%d",b);
 return 0;
 }
